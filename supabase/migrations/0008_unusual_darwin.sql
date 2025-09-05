4682545195938cf5ddeee3beff26e4814191b574
Pragyan Mehrotra <mehrotrapragyan@gmail.com>
2025-09-05 17:15:37 -0500

    irrelevant change

---
+++ supabase/migrations/0008_unusual_darwin.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "pets" ALTER COLUMN "breed" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "pets" ALTER COLUMN "weight" SET DATA TYPE integer;--> statement-breakpoint
+ALTER TABLE "pets" ALTER COLUMN "weight" SET NOT NULL;
